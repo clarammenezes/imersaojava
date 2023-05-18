4355b4cd6700a3a3221490ae1bec44c2217f3ce8
clarammenezes <clarammenezes@gmail.com>
2023-05-18 15:32:58 -0300

    aula 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,30 +8,37 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
 public class App {
+
     public static void main(String[] args) throws Exception {
-        // Fazer uma conexão HTTP e buscar os tops 250 filmes
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_20x0wh97";
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
-        // Parsear os dados que interessam (título, poster, classificação)
-        var parser  = new JsonParser();
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-
         // exibir e manipular os dados
+        var gerador = new GeradorDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            gerador.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
-
     }
 }
